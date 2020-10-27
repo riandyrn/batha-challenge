0ea2289ac2c3afbab008717369c1598d461a7cd5
Riandy Rahman Nugraha <riandyrn@gmail.com>
2020-10-27 09:52:08 +0300

    finish login functionality; work on progress;

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,9 @@
+const endpointURL = "https://80bwgdzl22.execute-api.ap-southeast-1.amazonaws.com/release"
 var app = new Vue({
     el: '#app',
     data: {
         state: "LOGIN_SCREEN",
+        access_key: ""
     },
     methods: {
         showSendEmailScreen() {
@@ -13,8 +15,32 @@ var app = new Vue({
         showGetUploadedFilesScreen() {
             this.state = "GET_UPLOADED_FILES_SCREEN";
         },
-        doLogin() {
-            alert("Hello World!");
+        async doLogin() {
+            // disable login button
+            document.getElementById("btn_login").disabled = true;
+            // execute http request
+            const response = await fetch(`${endpointURL}/sessions`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + this.access_key
+                }
+            });
+            // parse response
+            const respJSON = await response.json();
+            // handle error by giving alert to user
+            if (response.status != 200) {
+                if (respJSON.message) {
+                    alert(respJSON.message);
+                } else {
+                    alert("Something wrong happened, please try again later");
+                }
+                // enable login button
+                document.getElementById("btn_login").disabled = false;
+                return
+            }
+            // update state to send email screen
+            this.showSendEmailScreen();
         }
     }
 })
