1df0d8112710f2917777ca2152c0b0fd4872cad3
Riandy Rahman Nugraha <riandyrn@gmail.com>
2020-10-27 10:28:12 +0300

    finish send email screen feature; work on progress;

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,12 @@ var app = new Vue({
     el: '#app',
     data: {
         state: "LOGIN_SCREEN",
-        access_key: ""
+        access_key: "",
+        email: "",
+        name: "",
+        send_email_page: {
+            body_html: "<h1>Hello World!</h1>"
+        }
     },
     methods: {
         showSendEmailScreen() {
@@ -39,8 +44,40 @@ var app = new Vue({
                 document.getElementById("btn_login").disabled = false;
                 return
             }
+            // update email & name values
+            this.email = respJSON.data.email;
+            this.name = respJSON.data.name;
             // update state to send email screen
             this.showSendEmailScreen();
+        },
+        async doSendEmail() {
+            // disable send email button
+            document.getElementById("btn_email").disabled = true;
+            // execute http request
+            const response = await fetch(`${endpointURL}/emails`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': 'Bearer ' + this.access_key
+                },
+                body: JSON.stringify({
+                    body_html: this.send_email_page.body_html,
+                })
+            })
+            // parse response
+            const respJSON = await response.json();
+            // give response to user
+            if (response.status != 200) {
+                if (respJSON.message) {
+                    alert(respJSON.message);
+                } else {
+                    alert("Something wrong happened, please try again later");
+                }
+            } else {
+                alert(`Succesfully send email to ${this.email}`);
+            }
+            // enable send email button
+            document.getElementById("btn_email").disabled = false;
         }
     }
 })
